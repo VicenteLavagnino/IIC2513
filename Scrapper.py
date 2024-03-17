88c264765daa8ccadfad1d0108b08dd92df94598
VicenteLavagnino <vicente.lavagnino@uc.cl>
2024-03-17 19:40:49 -0300

    add comments to the code to explain what it does

--- Scrapper.py
+++ Scrapper.py
@@ -8,17 +8,29 @@ class Scrapper:
     def __init__(self, web_driver: WebDriver):
         self.web_driver = web_driver
 
+    # COMPLETAR
+    # Debe buscar el pokemon en la pagina
+    # Recibe el nombre del pokemon y no retorna nada
     def find_pokemon(self, nombre: str) -> None:
         pass
 
+    # COMPLETAR
+    # Debe buscar la informacion de cada pokemon en la pagina
+    # Recibe una lista de nombres de pokemones y retorna una lista de listas con la informacion de cada pokemon
     def extract_pokemon_info(self, pokemon_list: list[str]) -> list:
         info_pokemons = []
         return info_pokemons
     
+    # COMPLETAR
+    # Debe ordenar la informacion de los pokemones por peso
+    # Recibe una lista de listas con la info de cada pokemon y retorna una lista de listas con la info de cada pokemon ordenada
     def sort_by_weight(self, info: list) -> list:
         info_ordenada = []
         return info_ordenada
 
+    # COMPLETAR
+    # Debe escribir la informacion de los pokemones en un archivo csv (Incluyendo el header)
+    # Recibe una lista de listas con la info de cada pokemon y no retorna nada
     def write_csv(self, info: list) -> None:
         header = "NOMBRE;TIPOS;CATEGORIA;PESO;ALTURA\n"
         filename = 'pokemons.csv'
--- Web_driver.py
+++ Web_driver.py
@@ -14,38 +14,75 @@ class WebDriver:
         self.driver = None
         self.tabs = []
 
+    # COMPLETAR
+    # Deberas utilizar la librera webdriver para instanciar el driver y los tabs
+    # No tiene atributos, no retorna nada
     def initialize_driver(self) -> None:
         pass
 
+    # COMPLETAR
+    # Funcion para cargar una pagina
+    # Recibe un str, no retorna nada
     def load_page(self, page: str) -> None:
         pass
 
+    # COMPLETAR
+    # Funcion para abrir una nueva pestana, agregar un tab y actualizar el active_tab
+    # Recibe un str, no retorna nada
     def new_tab(self, page: str) -> None:
         pass
 
+    # COMPLETAR
+    # Funcion que actualiza el atributo active_tab y cambia de pestana del driver
+    # En caso que no queden pestanas, se debe resetar el driver
+    # Recibe un int, no retorna nada
     def change_tab(self, page_index: int) -> None: 
         pass
     
+    # COMPLETAR
+    # Funcion para cerrar una pestana, debe actualizar los atributos
+    # No recibe ni retorna nada
     def close_tab(self) -> None:
         pass
 
+    # COMPLETAR
+    # Funcion para hacer click en un elemento
+    # Recibe el tipo de busqueda y el valor, y hace click en el elemento. No retorna nada
     def click_element(self, by: By, value: str) -> None:
         pass
     
+    # COMPLETAR
+    # Funcion para encontrar un elemento
+    # Recibe el tipo de busqueda y el valor, y retorna el elemento
     def find_element(self, by: By, value: str) -> SeleniumWebElement:
         pass
 
+    # COMPLETAR
+    # Funcion para escribir en un elemento
+    # Recibe el tipo de busqueda, el valor y el texto a escribir, y escribe el texto en el elemento
     def write_in_element(self, by: By, value: str, text: str) -> None:
         pass
 
+    # COMPLETAR
+    # Funcion para extraer texto
+    # Recibe el tipo de busqueda y el valor, y retorna el texto del elemento
     def get_text(self, by: By, value: str) -> str:
         pass
     
+    # COMPLETAR
+    # Funcion para extraer atributo title
+    # Recibe el tipo de busqueda y el valor, y retorna el atributo title del elemento
     def get_title(self, by: By, value: str) -> str:
         pass
 
+    # COMPLETAR
+    # Funcion para extraer url
+    # Retorna la url de la pagina actual
     def get_url(self) -> str:
         pass
     
+    # COMPLETAR
+    # Funcion para cerrar el driver
+    # Cierra el driver y resetea el atributo a None
     def quit_driver(self) -> None:
         pass
